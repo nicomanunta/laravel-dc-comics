3ffcb08bcf19ada3e818414dc26a59ab9360bdd1
nicomanunta <nic.manun@gmail.com>
2024-02-19 15:13:42 +0100

    salvo nel db

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -39,7 +39,17 @@ class ComicController extends Controller
     public function store(Request $request)
     {
         $form_data= $request-> all();
-        
+
+        $new_comic = new Comic();
+        $new_comic-> title = $form_data['title'];
+        $new_comic-> description = $form_data['description'];
+        $new_comic-> thumb = $form_data['thumb'];
+        $new_comic-> price = $form_data['price'];
+        $new_comic-> series = $form_data['series'];
+        $new_comic-> sale_date = $form_data['sale_date'];
+        $new_comic-> type = $form_data['type'];
+        $new_comic-> save();
+
     }
 
     /**
