e2d8c59e7f1bd729fc6a980f33634d14bf52a1ec
nicomanunta <nic.manun@gmail.com>
2024-02-19 14:26:26 +0100

    richiamo store e aggiungo @csrf

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,5 +1,17 @@
 @extends('layout.app')
 
 @section('content')
-    
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Aggiungi un nuovo Fumetto</h1>
+            </div>
+            <div class="col-12">
+                <form action="{{ route('comics.store')}}" method="post">
+                    @csrf
+                    
+                </form>
+            </div>
+        </div>
+    </div>
 @endsection
